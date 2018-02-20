66fdebae58805f2ffb35c7a1aa0ca86e79824f3c
William Murphy <william@datatitian.com>
2018-02-20 10:00:25 -0600

    Last stable master before WebGLRenderer API change (with one added revert to fix text depth)

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.7.0 (Date 2017-12-04, Commit #9687531)');
+console.log('A-Frame Version: 0.7.0 (Date 2018-02-20, Commit #eee4e17)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
